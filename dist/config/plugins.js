3d4017b53c30aab3d8991f544e40089ba48666fd
Dustin Schau <dustinschau@gmail.com>
2017-03-03 15:53:09 -0600

    feat: add define plugin for environment detection

--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -4,6 +4,26 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
+var getEnvironment = function getEnvironment() {
+  var env = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+  var keys = Object.keys(env);
+  var map = {
+    development: 'development',
+    dev: 'development',
+    test: 'test',
+    production: 'production',
+    prod: 'production'
+  };
+
+  for (var i = 0; i < keys.length; i++) {
+    if (map[key]) {
+      return map[key];
+    }
+  }
+  return map.production;
+};
+
 module.exports = function plugins(_ref, env, ExtractTextPlugin) {
   var dirname = _ref.dirname;
 
@@ -12,5 +32,7 @@ module.exports = function plugins(_ref, env, ExtractTextPlugin) {
   }), new HtmlWebpackPlugin({
     template: path.join(dirname, 'src/public/index.pug'),
     chunksSortMode: 'dependency'
+  }), new webpack.DefinePlugin({
+    'process.env.NODE_ENV': getEnvironment(env)
   })];
 };
--- src/config/plugins.js
+++ src/config/plugins.js
@@ -2,6 +2,24 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
+const getEnvironment = (env = {}) => {
+  const keys = Object.keys(env);
+  const map = {
+    development: 'development',
+    dev: 'development',
+    test: 'test',
+    production: 'production',
+    prod: 'production'
+  };
+
+  for ( let i = 0; i < keys.length; i++ ) {
+    if ( map[key] ) {
+      return map[key];
+    }
+  }
+  return map.production;
+};
+
 module.exports = function plugins({ dirname }, env, ExtractTextPlugin) {
   return [
     new webpack.ContextReplacementPlugin(
@@ -15,6 +33,9 @@ module.exports = function plugins({ dirname }, env, ExtractTextPlugin) {
     new HtmlWebpackPlugin({
       template: path.join(dirname, 'src/public/index.pug'),
       chunksSortMode: 'dependency'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': getEnvironment(env)
     })
   ];
 }
