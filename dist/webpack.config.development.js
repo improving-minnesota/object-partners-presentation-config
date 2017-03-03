40204f2d9413bd55fb272f091bff6038a0131175
Dustin Schau <dustinschau@gmail.com>
2017-03-03 14:52:14 -0600

    feat: add NamedModulesPlugin

--- dist/webpack.config.development.js
+++ dist/webpack.config.development.js
@@ -4,5 +4,6 @@ module.exports = {
   devtool: 'eval-source-map',
   output: {
     publicPath: '/'
-  }
+  },
+  plugins: [new webpack.NamedModulesPlugin()]
 };
--- src/webpack.config.development.js
+++ src/webpack.config.development.js
@@ -2,5 +2,8 @@ module.exports = {
   devtool: 'eval-source-map',
   output: {
     publicPath: '/'
-  }
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin()
+  ]
 };
