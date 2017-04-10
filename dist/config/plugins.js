01a33dd65e32b702f54dcca22be3fb6f1e7bf202
Dustin Schau <dustinschau@gmail.com>
2017-04-10 17:40:04 -0500

    chore: commit dist

--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -28,7 +28,7 @@ var getEnvironment = function getEnvironment() {
 module.exports = function plugins(_ref, env, ExtractTextPlugin) {
   var dirname = _ref.dirname;
 
-  return [new webpack.ContextReplacementPlugin(/angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/, dirname), ExtractTextPlugin, new webpack.optimize.CommonsChunkPlugin({
+  return [new webpack.ContextReplacementPlugin(/angular(\\|\/)core(\\|\/)@angular/, path.resolve(dirname, 'src')), ExtractTextPlugin, new webpack.optimize.CommonsChunkPlugin({
     name: ['vendor', 'polyfills']
   }), new HtmlWebpackPlugin({
     template: path.join(dirname, 'src/public/index.pug'),
