fd674fd80eff76b80ecf4a20da48dd1cef179989
Dustin Schau <dustinschau@gmail.com>
2017-04-17 12:59:28 -0500

    chore: remove extraneous console log

--- __tests__/webpack.config.base.js
+++ __tests__/webpack.config.base.js
@@ -1,7 +1,6 @@
 import base from '../src/webpack.config.base';
 
 test('it has expected properties', () => {
-  console.log(base({ dirname: __dirname }));
   expect(Object.keys(base({ dirname: __dirname }))).toEqual([
     'entry',
     'output',
