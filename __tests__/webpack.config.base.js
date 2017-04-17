2635ae39c86dbcc28a84589e79cbe09058b15ce6
Dustin Schau <dustinschau@gmail.com>
2017-04-17 12:58:14 -0500

    feat: allow extend config to be a function

--- __tests__/webpack.config.base.js
+++ __tests__/webpack.config.base.js
@@ -1,6 +1,7 @@
 import base from '../src/webpack.config.base';
 
 test('it has expected properties', () => {
+  console.log(base({ dirname: __dirname }));
   expect(Object.keys(base({ dirname: __dirname }))).toEqual([
     'entry',
     'output',
